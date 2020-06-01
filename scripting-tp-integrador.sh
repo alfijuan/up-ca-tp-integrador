41eb1f5a4c11d3f5747c04909cfd0b5b355c2657
Juan Alfieri <alfieri.juan@gmail.com>
2020-06-01 10:48:07 -0300

    add new functions and menu options

--- scripting-tp-integrador.sh
+++ scripting-tp-integrador.sh
@@ -3,7 +3,7 @@ clear
 
 while true;do
 	menu()
-	read -p "Selecccione una opcion" num
+	read -p "Selecccione una opcion: " num
 	case $num in
 		1) fibonacci() ;;
 
@@ -11,9 +11,15 @@ while true;do
 
 		3) is_palindrome() ;;
 
+		4) file_lines_length() ;;
+		
+		5) order_numbers() ;;
+		
+		6) show_files_directory() ;;
+		
 		7) exit_menu() break ;;
-
-		8) echo "Opcion incorrecta"
+		
+		8) echo "Opcion incorrecta" ;;
 	esac
 done
 
@@ -24,6 +30,8 @@ function menu(){
 	echo "2. Pedir un numero y mostrar por pantalla ese numero en forma invertida"
 	echo "3. Pedir una cadena de caracteres y evaular si es palindomo o no"
 	echo "4. Pedir el path a un archivo de texto y mostrar por pantalla la cantidad de lineas que tiene"
+	echo "5. Pedir el ingreso de 5 numeros enteros y mostrarlos por pantalla en forma ordenada"
+	echo "6. Pedir el path a un directorio y mostrar por pantalla cuantos archivos de cada tipo contiene"
 	echo "7. Salir"
 
 }
@@ -31,8 +39,7 @@ function menu(){
 function fibonacci(){
 	aux1=0 #Primero de la serie
 	aux2=1 #Segundo de la serie
-	echo "Ingrese el numero para ver la serie: "
-	read val
+	read -p "Ingrese el numero para ver la serie: " val
 	echo "La serie es: "
 	for ((i=0; i<$val; i++))
 	do
@@ -44,9 +51,22 @@ function fibonacci(){
 }
 
 function revert_num(){
-	echo "Ingrese un numero a revertir"
-	read num
+	read -p "Ingrese un numero a revertir: " num
 	echo $num | rev
+}
+
+function is_palindrome(){
+	read -p "Ingrese cualquier tipo de texto: " $text
+	
+}
+
+function file_lines_length(){
+}
+
+function order_numbers(){
+}
+
+function show_files_directory(){
 }
 
 function exit_menu(){
